279fade5861d9f2cb5be0ec850b2cd923fa7fa6c
Phil Belyaev <belyaevfil@gmail.com>
2020-09-03 13:38:56 +0500

    created development part of webpack config

---
+++ webpack.dev.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const common = require("./webpack.common");
+const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+    mode: "development",
+    devServer: {
+        port: 3000,
+        hot: true,
+        overlay: true,
+        open: true
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    }
+})

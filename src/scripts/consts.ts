61373111fe4f22d68d7cc6219940e01a7bdfbe93
Phil Belyaev <belyaevfil@gmail.com>
2020-09-12 12:24:23 +0500

    created last commit before deploy

--- src/scripts/consts.ts
+++
@@ -1,8 +0,0 @@
-const STORAGE_NAME: string = 'todos';
-const availablePlaceholders: string[] = [
-  'Feed the fish...',
-  'Clean the room...',
-  'Do homework...'
-];
-
-export { STORAGE_NAME, availablePlaceholders };
